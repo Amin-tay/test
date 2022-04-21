8bfeb4e96816531c5fb77bfbda556bb2744a4645
Amin-tay <mohammadamintayebi1@gmail.com>
2022-04-21 15:35:24 +0430

    add: hex and feature

--- src/main/java/enums/HEX.java
+++ src/main/java/enums/HEX.java
@@ -1,2 +1,50 @@
-package Enums;public enum HEX {
+package Enums;
+
+public enum HEX {
+    DESERT (0,0,0,-0.33,1),
+    MEADOW(2,0,0,-0.33,1),
+    HILL(0,2,0,+0.25,2),
+    MOUNTAIN(0,0,0,0,-1),
+    OCEAN(0,0,0,0,-1),
+    PLAIN(1,1,0,-0.33,1),
+    SNOW (0,0,0,-0.33,1),
+    TUNDRA(1,0,0,-0.33,1);
+
+
+
+    private int food;
+    private int production;
+    private int gold;
+    private double battleEfficacy;
+    private int MP;
+
+    HEX(int food, int production, int gold, double battleEfficacy, int MP) {
+        this.food = food;
+        this.production = production;
+        this.gold = gold;
+        this.battleEfficacy = battleEfficacy;
+        this.MP = MP;
+
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public int getProduction() {
+        return production;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public double getBattleEfficacy() {
+        return battleEfficacy;
+    }
+
+    public int getMP() {
+        return MP;
+    }
+
 }
--- src/main/java/enums/LandFeature.java
+++ src/main/java/enums/LandFeature.java
@@ -1,2 +1,51 @@
-package Enums;public enum LandFeature {
+package Enums;
+
+public enum LandFeature {
+    VALLEY(2, 0, 0, -0.33, 1, new HEX[]{HEX.DESERT, HEX.HILL, HEX.MEADOW, HEX.PLAIN}),
+    JUNGLE(1, 1, 0, 0.25, 2, new HEX[]{HEX.DESERT, HEX.HILL, HEX.MEADOW, HEX.PLAIN}),
+    ICE(0, 0, 0, 0, -1, new HEX[]{HEX.TUNDRA}),
+    MASSIVE_FOREST(1, -1, 0, 0.25, 2, new HEX[]{HEX.HILL, HEX.PLAIN, HEX.MEADOW}),
+    LAGOON(-1, 0, 0, -0.33, 2, null),
+    OASIS(3, 0, 1, -0.33, 1, null);
+
+    private int food;
+    private int production;
+    private int gold;
+    private double battleEfficacy;
+    private int MP;
+    private HEX[] canBeFindOn;
+
+    LandFeature(int food, int production, int gold, double battleEfficacy, int MP, HEX[] canBeFindOn) {
+        this.food = food;
+        this.production = production;
+        this.gold = gold;
+        this.battleEfficacy = battleEfficacy;
+        this.MP = MP;
+        this.canBeFindOn = canBeFindOn;
+
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public int getProduction() {
+        return production;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public double getBattleEfficacy() {
+        return battleEfficacy;
+    }
+
+    public int getMP() {
+        return MP;
+    }
+
+    public HEX[] getCanBeFindOn() {
+        return canBeFindOn;
+    }
 }
